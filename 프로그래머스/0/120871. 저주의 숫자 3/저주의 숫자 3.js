82bbc122091f511422fc1b3a217456965c939ac8
eunjin <whdmswls9207@gmail.com>
2025-05-20 18:39:29 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,12 +1,4 @@
 function solution(n) {
-    const isValid = (num) => num % 3 !== 0 && !String(num).includes('3');
-    
-    const numList = [0]
-    let maxNum = 101
-    for ( let i = 0 ; i<maxNum ; i++){
-        if (isValid(i)) numList.push(i)
-        else maxNum++
-    }
-    return numList[n];
-
+    var answer = 0;
+    return answer;
 }
